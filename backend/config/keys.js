af02319a2aec0da5148d65be81ca297ef3d24828
nirasha9806 <nirasha.chamudini@gmail.com>
2020-09-13 15:28:01 +0530

    initial commit

---
+++ backend/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://nirasha:nirasha123@cluster.ly4e9.mongodb.net/AFIO?retryWrites=true&w=majority'
+}
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+//BodyParser Middleware
+app.use(bodyParser.json());
+
+//DB config
+const db = require('./config/keys').mongoURI;
+
+//connect to Mongo
+mongoose
+    .connect(db)
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+    
+    const port  = process.env.PORT || 5000;
+
+
+    app.listen(port, () => console.log('Server started on port ' +port));
