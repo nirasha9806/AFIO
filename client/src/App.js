7ffd96f33003c0ff688085303041b3393b22e4e0
nirasha9806 <nirasha.chamudini@gmail.com>
2020-09-14 10:06:05 +0530

    delivery updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+
+import CardPay from './Components/Payment/CardPay.js';
 
 import DeliveryForm from './Components/Delivery/DeliveryDetailsInsert'
 
 function App() {
   return (
     <Router>
+
+       <Route path="/cardPay" component={CardPay} />
         <Route exact path ="/" component={DeliveryForm}/>
         
       </Router>
