3b0154621d6e0573f4587201c59cd9cc91882716
Kaveena99 <kaveenadewminijayasekara@gmail.com>
2020-09-18 21:40:45 +0530

    voucher crud

--- client/src/App.js
+++ client/src/App.js
@@ -42,8 +42,6 @@ import OrderHistory from './Components/Delivery/OrderHistory'
 function App() {
   return (
     <Router>
-
-
       
       <Route path="/cardPay" component={CardPay} />
       <Route path="/createProduct" component={CreateProduct} />
@@ -62,15 +60,17 @@ function App() {
       <Route path="/user/profile/:email/:password" component={UserDetails} />
       <Route exact path="/user/table" component={Viewcategory} />
       <Route exact path="/edit/:id" component={EditFormPage} />
-      <Route path="/EditVoucher/:id" component={EditVoucher} />
-      <Route path="/list" component={ListVoucher} />
-      <Route path="/voucher" component={CreateVoucher} />
+
       <Route path ="/deliveryInsert/:id" component={DeliveryForm}/>
       <Route path ="/displayDelivery/:id" component={DisplayDeliveryDetails}/>
       <Route path="/deliveryAdmin" component={Deliveries}/>
       <Route path="/update/:id" component={UpdateStatus}/>
       <Route path="/checkRequests" component={StatusRequests}/>
       <Route path="/orderHistory" component={OrderHistory}/>
+
+      <Route path="/create" component={CreateVoucher} />
+      <Route path="/EditVoucher/:id" component={EditVoucher} />
+      <Route path="/list" component={ListVoucher} />
 
       </Router>
 
